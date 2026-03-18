bb9716f43e663c619e3577ee40910bca8fc0bcc1
Alejandra Artica <artsosaalejandra@gmail.com>
2026-03-18 01:30:09 -0600

    Express

--- API/index.js
+++ API/index.js
@@ -256,7 +256,7 @@ app.use("/api", backupRoutes);
 const path = require("path");
 
 // Servir frontend
-app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(Express.static(path.join(__dirname, "../frontend/dist")));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
