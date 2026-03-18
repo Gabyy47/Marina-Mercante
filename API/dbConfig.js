6eb2437613b8063ff4cbdd2bf5cbfb0a6c05036b
Alejandra Artica <artsosaalejandra@gmail.com>
2026-03-18 00:49:16 -0600

    URLPLUBLIC

--- API/dbConfig.js
+++ API/dbConfig.js
@@ -1,10 +1,12 @@
 // API/dbConfig.js
+
+const url = new URL(process.env.MYSQL_PUBLIC_URL);
 const DB_CONFIG = {
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  host: url.hostname,
+  user: url.username,
+  password: url.password,
+  database: url.pathname.replace("/", ""),
+  port: url.port
 };
 
 module.exports = DB_CONFIG;
